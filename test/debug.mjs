8dbb9f478afd5d635c4792ebe6cfa53b0337559e
X3ZvaWQ <x3zvawq@gmail.com>
2024-02-13 16:21:25 +0800

    fix: unit test

--- test/debug.mjs
+++ test/debug.mjs
@@ -3,33 +3,13 @@ import { LUA_REGISTRYINDEX, Lua } from '../dist/index.js';
 
 const lua = await Lua.create();
 
-const value = lua
-    .doStringSync(
-        `
-        local obj1 = {
-            hello = 'world',
-        }
-        obj1.self = obj1
-        local obj2 = {
-            5,
-            hello = 'everybody',
-            array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            fn = function()
-                return 'hello'
-            end
-        }
-        obj2.self = obj2
-        return { obj1 = obj1, obj2, name = 123 }
-    `,
-    )
-    .$detach({ dictType: 1 });
+const obj = {};
+lua.ctx.obj = obj;
 
-const obj = [, [, 5]];
-obj[1]['hello'] = 'everybody';
-obj[1]['array'] = [, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-obj[1]['fn'] = value[1]['fn'];
-obj['obj1'] = {
-    hello: 'world',
-};
-obj['obj1'].self = obj['obj1'];
-obj[1].self = obj[1];
+const o = lua.ctx.obj;
+o.name = 114514;
+console.log(o.name);
+
+lua.doStringSync(`
+    print(obj.name)
+`);
